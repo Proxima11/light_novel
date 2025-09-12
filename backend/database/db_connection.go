4ee8e4090d25bed56f17127da56ad22ce6059535
Proxima11 <c14210139@john.petra.ac.id>
2025-09-12 09:39:44 +0700

    change db connection

--- backend/database/db_connection.go
+++ backend/database/db_connection.go
@@ -1,17 +1,29 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func main() {
-	dsn := "host=localhost user=postgres password=xxxx dbname=xxxxx port=5432 sslmode=disable"
+var DB *gorm.DB
+
+func ConnectPostgreDB() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Gagal koneksi:", err)
 	}
-	log.Println("Berhasil connect dengan GORM!")
+	DB = db
+	log.Println("Berhasil connect dengan database!")
 }
